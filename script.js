f8d7b81b6b5f8f6e8d76d9997208c342e9e64a71
Renishkar J <107568859+renish47@users.noreply.github.com>
2022-10-21 15:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,74 @@
-let submit = document.getElementById('submit');
-let fname = document.getElementById('first-name');
-let lname = document.getElementById('last-name');
-let add = document.getElementById('address');
-let pin = document.getElementById('pincode');
-let state = document.getElementById('state');
-let country = document.getElementById('country');
 
-let food1 = document.getElementById('food1');
-let food2 = document.getElementById('food2');
-let food3 = document.getElementById('food3');
-let food4 = document.getElementById('food4');
-let food5 = document.getElementById('food5');
-let foods = [food1,food2,food3,food4,food5]
-let food ='';
+function getData(event){
+  event.preventDefault();
 
-let gender1 = document.getElementById('gender1');
-let gender2 = document.getElementById('gender2');
-let gender='';
+  let fname = event.target.firstName.value;
+  let lname = event.target.lastName.value;
+  let address = event.target.address.value;
+  let pin = event.target.pinCode.value;
+  let state = event.target.state.value; 
+  let country = event.target.country.value;
+  let gender= event.target.gender.value;
+  let checkCount=0;
+  let foods =checkedFoods();
+
+  updateDatabase();
 
 
+  function checkedFoods(){
+    let food1= event.target.food1;
+    let food2= event.target.food2;
+    let food3= event.target.food3;
+    let food4= event.target.food4;
+    let food5= event.target.food5;
+    let foods=[food1,food2,food3,food4,food5]
+    let checkedFood='';
 
-
-let tbody= document.getElementById('tbody')
-
-submit.addEventListener('click',()=>{
-
-  
-  //gender
-  if(gender1.checked){
-    gender ="Male"
-  }
-  else if(gender2.checked)
-    gender = "Female"
-
-  //food
-  for(let x of foods){
-    if(x.checked)
-      food= food + x.value +", "
-  }
-  food = food.slice(0,-2)
-
-  let check = [fname.value,lname.value,add.value,pin.value,gender,food,state.value,country.value]
-
-  console.log(check)
-
- if(!check.includes('')){
-   createTable();
-   document.getElementById('warning').innerText = ''
-   window.scrollTo(0,0)
-   clear();
- }
-  else{
-    document.getElementById('warning').innerText = '*Need to fill all the boxes'
-    setTimeout(()=>document.getElementById('warning').innerText = '',2000)
+    for(let food of foods){
+      if(food.checked){
+        checkedFood+=food.value+', ';
+        checkCount++;
+      }
+    }
+    return checkedFood.slice(0,-2)
   }
   
-})
+  function updateDatabase(){
+    let check = [fname,lname,address,pin,gender,country,state,foods]
+    console.log(checkCount)
+    if(!check.includes('') && checkCount>1){
+      createNewRow();
+      document.getElementById('warning').innerText = ''
+      window.scrollTo(0,0)
+      document.getElementById('form').reset();
+    }
+    else{
+      document.getElementById('warning').innerText = '*Need to fill all the Fields'
+      if(checkCount<2)
+        document.getElementById('foodReq').style.color ='Red' ; 
+      setTimeout(()=>{
+        document.getElementById('warning').innerText = '';
+        document.getElementById('foodReq').style.color ='black';
+      },2000)
 
-function createTable(){
-  let tr = document.createElement('tr')
-  tr.innerHTML = `
-                  <td>${fname.value}</td>
-                  <td>${lname.value}</td>
-                  <td>${add.value}</td>
-                  <td>${pin.value}</td>
-                  <td>${gender}</td>
-                  <td>${food}</td>
-                  <td>${state.value}</td>
-                  <td>${country.value}</td>
-                  `
+    }
+  }
+  
+  function createNewRow(){
+    let tbody= document.getElementById('tbody')
+    let tr = document.createElement('tr')
+    tr.innerHTML = `
+                    <td>${fname}</td>
+                    <td>${lname}</td>
+                    <td>${address}</td>
+                    <td>${pin}</td>
+                    <td>${gender}</td>
+                    <td>${foods}</td>
+                    <td>${state}</td>
+                    <td>${country}</td>
+                    `
+    tbody.append(tr);
+  }
 
-  tbody.append(tr);
 }
 
-function clear(){
-  food =''
-  gender=''
-  fname.value=''
-  lname.value=''
-  pin.value=''
-  add.value=''
-  state.value=''
-  country.value=''
-  var checkBox = document.getElementsByName("food");
-  for(var i=0;i<checkBox.length;i++)
-     checkBox[i].checked = false;
-
-  var radio = document.getElementsByName("gender");
-  for(var i=0;i<radio.length;i++)
-    radio[i].checked = false;
-
-}
